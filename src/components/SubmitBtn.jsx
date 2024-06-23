171fc58852a488dbe32fa8e0e90d1978ab941c9c
Elharfali <elharfali.youssef.20@gmail.com>
2024-06-23 12:53:49 +0100

    Add Login and Register pages

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useNavigation } from 'react-router-dom'
+
+const SubmitBtn = ({ text }) => {
+  const navigate = useNavigation()
+  const isSubmitting = navigate.state === 'submitting'
+
+  return (
+      <button type='submit' className='btn btn-primary btn-block' disabled={isSubmitting}>
+          {
+        isSubmitting ? (
+          <>
+            <span className="loading loading-spinner"></span>
+            sending...
+          </>
+        ): (
+            text || 'submit'         
+            )
+          }
+    </button>
+  )
+}
+
+export default SubmitBtn
