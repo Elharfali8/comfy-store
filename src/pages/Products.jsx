f926c0309d5af88f3b1f57bace62eab927934502
Elharfali <elharfali.youssef.20@gmail.com>
2024-06-28 11:22:58 +0100

    Add Filters to products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,12 +3,16 @@ import { customFetch } from '../utils'
 import { useLoaderData } from 'react-router-dom'
 import { Filters, PaginationContainer, ProductsContainer } from '../components'
 
-export const loader = async ({request}) => {
-  const response = await customFetch('/products')
+export const loader = async ({ request }) => {
+  const params = Object.fromEntries([...new URL(request.url).searchParams.entries()])
+  
+  const response = await customFetch('/products', {
+    params
+  })
   const products = response.data.data
   const meta = response.data.meta
 
-  return {products, meta}
+  return {products, meta, params}
 }
 
 const Products = () => {
